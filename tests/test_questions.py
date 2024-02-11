7e8bb946958382deddcc3e9be1b8b199d419c435
sewerynszydelko <seweryn494a@gmail.com>
2024-02-11 12:49:18 +0100

    Test: change adding path directly to Class

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -5,16 +5,16 @@ path = "C:\\Users\\5792\\Desktop\\Quiz_0\\scripts\\Questions_Answears.json"
 
 
 def test_load_questions():
-    test_question_obj = Questions()
+    test_question_obj = Questions(path)
 
-    test_question_obj.load_questions(path)
+    test_question_obj.load_questions()
 
     assert test_question_obj
 
 
 def test_display_questions():
-    test_question_obj = Questions()
-    test_question_obj.load_questions(path)
+    test_question_obj = Questions(path)
+    test_question_obj.load_questions()
 
     test_question_obj.display_question_and_answaers()
 
